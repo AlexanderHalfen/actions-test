4266efd2faaccbb026e614225152bb584274bd1f
Philipp-Alexander Halfen <phalfen@avoplan.de>
2024-05-18 15:33:55 +0200

    envs

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+
+	@Value("${test.value}")
+	String value;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		// Your custom code here
+		System.out.println("Value: " + value);
+	}
+
+
 
 }
